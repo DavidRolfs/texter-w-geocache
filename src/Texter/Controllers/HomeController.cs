0bdb68db7fd912e1842d8b70b1aa1a9560aa24fc
David Rolfs and Pete Lazuran <david+pete@>
2017-08-22 16:27:35 -0700

    working app

---
+++ src/Texter/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Texter.Models;
+
+
+namespace Texter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetMessages()
+        {
+            var allMessages = Message.GetMessages();
+            return View(allMessages);
+        }
+
+        public IActionResult SendMessage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SendMessage(Message newMessage)
+        {
+            newMessage.Send();
+            return RedirectToAction("Index");
+        }
+
+
+        public IActionResult FindCoords()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult FindCoords(double Lat, double Lng)
+        {
+            //Geocache thisGeocache = new Geocache();
+            var foundAddress = Geocache.FindLocation(Lat, Lng);
+            return RedirectToAction("Show", foundAddress);
+        }
+
+        public IActionResult Show(Geocache geocache)
+        {
+             
+             return View(geocache);
+        }
+
+    }
+}
---
+++ src/Texter/Models/Geocache.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using RestSharp.Authenticators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Texter.Models;
+using Newtonsoft.Json.Linq;
+
+namespace Texter.Models
+{
+    public class Geocache
+    {
+        public string formatted_address { get; set; }
+
+        public static Geocache FindLocation(double Lat, double Lng)
+        {
+            var client = new RestClient("https://maps.googleapis.com/maps/api/geocode/");
+            var request = new RestRequest("json?latlng=" + Lat + ", " + Lng + "&key=" + EnvironmentVariables.geoKey);
+        //client.Authenticator = new HttpBasicAuthenticator(EnvironmentVariables.geoKey);
+        var response = new RestResponse();
+        Task.Run(async() =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+    JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            Console.WriteLine(jsonResponse["results"][0]);
+            Console.WriteLine(jsonResponse["results"][0]["formatted_address"]);
+            var CoordList = JsonConvert.DeserializeObject<Geocache>(jsonResponse["results"][0].ToString());
+            return CoordList;
+        }
+
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response => {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
